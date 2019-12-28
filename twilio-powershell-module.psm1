9ca13669dc8fac0c73dae51ca8a5c1e1f2f28702
Jeff Brown Tech <jeff@jeffbrown.tech>
2019-12-28 12:46:59 -0800

    Added Send-TwilioSMS function

--- twilio-powershell-module.psm1
+++ twilio-powershell-module.psm1
@@ -5,29 +5,59 @@ function Get-TwilioAccountSID {
 function Set-TwilioAccountSID {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(Mandatory)]
         [string]
         $SID
     )
 
     $Script:TWILIO_ACCOUNT_SID = $SID
+
+    # If auth token is available, go ahead and create the full cred object
+    if ($null -ne $Script:TWILIO_AUTH_TOKEN) {
+        Set-TwilioCredentials -SID $Script:TWILIO_ACCOUNT_SID -AuthToken $Script:TWILIO_AUTH_TOKEN
+    }
 }
 
 function Set-TwilioAuthToken {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(Mandatory)]
         [string]
         $AuthToken
     )
 
     $Script:TWILIO_AUTH_TOKEN = $AuthToken
+    
+    # If auth token is available, go ahead and create the full cred object
+    if ($null -ne $Script:TWILIO_ACCOUNT_SID) {
+        Set-TwilioCredentials -SID $Script:TWILIO_ACCOUNT_SID -AuthToken $Script:TWILIO_AUTH_TOKEN
+    }
+}
+
+function Set-TwilioCredentials {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $SID,
+
+        [Parameter(Mandatory)]
+        [string]
+        $AuthToken
+    )
+
+    # Reference: https://www.twilio.com/docs/usage/tutorials/how-to-make-http-basic-request-twilio-powershell
+    $Script:TWILIO_CREDS = New-Object System.Management.Automation.PSCredential($SID, ($AuthToken | ConvertTo-SecureString -AsPlainText -Force))
+}
+
+function Get-TwilioPhoneNumber {
+    return $Script:TWILIO_PHONE_NUMBER
 }
 
 function Set-TwilioPhoneNumber {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(Mandatory)]
         [string]
         $PhoneNumber
     )
@@ -36,5 +66,35 @@ function Set-TwilioPhoneNumber {
 }
 
 function Send-TwilioSMS {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $ToPhoneNumber,
 
+        [Parameter()]
+        [string]
+        $FromPhoneNumber = $Script:TWILIO_PHONE_NUMBER,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Message
+    )
+
+    if ($PSBoundParameters.ContainsKey('FromPhoneNumber')) {
+        $body = @{
+            From = $FromPhoneNumber
+            To = $ToPhoneNumber
+            Body = $Message
+        }
+    }
+    else {
+        $body = @{
+            From = $Script:TWILIO_PHONE_NUMBER
+            To = $ToPhoneNumber
+            Body = $Message
+        }
+    }    
+    
+    Invoke-RestMethod -Method POST -Uri "https://api.twilio.com/2010-04-01/Accounts/$Script:TWILIO_ACCOUNT_SID/Messages.json" -Credential $Script:TWILIO_CREDS -Body $body
 }
