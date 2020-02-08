0cf6126441a91619be2e8b9f91e477ac5054bde9
Jeff Brown Tech <jeff@jeffbrown.tech>
2020-02-07 23:17:44 -0800

    added installing psscriptanalyzer to module

--- tests/InvokeScriptAnalyzer.ps1
+++ tests/InvokeScriptAnalyzer.ps1
@@ -1,3 +1,7 @@
+Install-Module -Name PSScriptAnalyzer -Repository PSGallery -Force -Verbose
+
+Import-Module -Name PSScriptAnalyzer -Force
+
 $analyzerErrors = Invoke-ScriptAnalyzer -Path "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)twilio-powershell-module.psm1" -Severity Error
 
 if ($analyzerErrors.Count -gt 0)
