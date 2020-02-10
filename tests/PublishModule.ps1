959cab11d5e73e1a4095c877dd6f0fe2f0cf3ea5
Jeff Brown Tech <jeff@jeffbrown.tech>
2020-02-10 05:57:11 -0800

    trying pipeline variable groups

--- tests/PublishModule.ps1
+++ tests/PublishModule.ps1
@@ -1,1 +1,16 @@
-Publish-Module -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -NuGetApiKey $($env:PowerShellGalleryAPI) -WhatIf -Verbose -ErrorAction STOP
+#Publish-Module -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -NuGetApiKey $($PowerShellGalleryAPI) -WhatIf -Verbose -ErrorAction STOP
+
+Write-Host "Try 1: " -NoNewline
+Write-Host $PowerShellGalleryAPI
+
+Write-Host "Try 2: " -NoNewline
+Write-Host $(PowerShellGalleryAPI)
+
+Write-Host "Try 3: " -NoNewline
+Write-Host $($PowerShellGalleryAPI)
+
+Write-Host "Try 4: " -NoNewline
+Write-Host $env:PowerShellGalleryAPI
+
+Write-Host "Try 5: " -NoNewline
+Write-Host $($env:PowerShellGalleryAPI)
